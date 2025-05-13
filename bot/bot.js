7de1cd15bfaee67b7418a51d9111eeccae95b600
danikkkkk12 <danilmalyk20@gmail.com>
2025-05-14 01:27:04 +0300

    code bot

--- bot/bot.js
+++ bot/bot.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
-const { Telegraf, Markup } = require('telegraf');
+const { Telegraf } = require('telegraf');
+
+const commands = require('./commands.js');
+const events = require('./events.js');
+
+if (!process.env.TELEGRAM_BOT_TOKEN) {
+    console.error("❌ Ошибка: TELEGRAM_BOT_TOKEN не найден!");
+    process.exit(1);
+}
 
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
-bot.start(startCommand);
-
-setupEvents(bot);
+bot.start(commands.startCommand);
+bot.on('new_chat_members', events.userJoined);
 
 bot.launch()
-  .then(() => console.log('Бот запущен и работает!'))
-  .catch(err => console.error('Ошибка запуска:', err));
-
-module.exports = bot;
+    .then(() => console.log("✅ Бот запущен и работает!"))
+    .catch(err => console.error("❌ Ошибка запуска:", err));
