e17e2066ebaad4d83b0ed8c297b3999a45e8d8ff
danikkkkk12 <danilmalyk20@gmail.com>
2025-05-13 23:47:25 +0300

    add html and css code

---
+++ web/js/partials-loader.js
@@ -0,0 +1,17 @@
+function loadPartial(id, url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById(id).innerHTML = html;
+    })
+    .catch(error => {
+      console.error(`Ошибка загрузки ${url}:`, error);
+    });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  loadPartial('main-content', 'partials/main.html');
+  loadPartial('balance-content', 'partials/balance.html');
+  loadPartial('buy-gift-content', 'partials/buy-gift.html');
+  loadPartial('inventory-content', 'partials/inventory.html');
+});
+app.use(express.static("public"));
